5b72f0dcccde0026f2ed692b59a135a9052b2726
Michal <michalzvu25@gmail.com>
2025-08-07 11:11:44 +0300

    pagination component

---
+++ nepeto-frontend/src/components/PaginatedList.js
@@ -0,0 +1,38 @@
+import { Box, Pagination } from "@mui/material";
+import React, { useState } from "react";
+
+export default function PaginatedList(props) {
+  const { children, itemsPerPage } = props;
+
+  const [page, setPage] = useState(1);
+  const count = React.Children.count(children);
+
+  const pageCount = Math.ceil(count / itemsPerPage);
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  const visibleChildren = React.Children.toArray(children).slice(
+    startIndex,
+    endIndex
+  );
+
+  const handleChange = (e, value) => {
+    setPage(value);
+  };
+
+  return (
+    <Box>
+      {visibleChildren}
+
+      <Box display="flex" justifyContent="center" mt={2}>
+        <Pagination
+          count={pageCount}
+          page={page}
+          onChange={handleChange}
+          color="primary"
+          size="small"
+        />
+      </Box>
+    </Box>
+  );
+}
